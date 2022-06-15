7cff68df86c3a4a6c9b45d439738b510ab6c82b3
gill <gillsukhdeep452@gmail.com>
2022-06-15 23:00:59 +0530

    fixed indentation issue

--- Two Sum/two-sum.go
+++ Two Sum/two-sum.go
@@ -5,21 +5,21 @@ import "fmt"
 func twoSum(nums []int, target int) []int {
     m := make(map[int]int)
 
-	for i, n := range nums {
-		var req = target - n
+    for i, n := range nums {
+        var req = target - n
 
-		if val, ok := m[req]; ok {
-			return []int{val, i}
-		}
+        if val, ok := m[req]; ok {
+            return []int{val, i}
+        }
 
-		m[n] = i
-	}
+        m[n] = i
+    }
 
-	return []int{0, 0}
+    return []int{0, 0}
 }
 
 func main() {
-	arr := []int{2,7,11,15} // example
-	ans := twoSum(arr, 9)
-	fmt.Printf("%v", ans)
+    arr := []int{2,7,11,15} // example
+    ans := twoSum(arr, 9)
+    fmt.Printf("%v", ans)
 }
