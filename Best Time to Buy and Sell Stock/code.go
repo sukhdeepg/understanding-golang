202fbe128a4261af1179629af051a3a64a45379c
gill <gillsukhdeep452@gmail.com>
2022-06-21 20:58:20 +0530

    added code for Best Time to Buy and Sell Stock

---
+++ Best Time to Buy and Sell Stock/code.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
+
+func maxProfit(prices []int) int {
+    minval, maxprofit := math.MaxInt32, 0
+
+    for _, price := range prices {
+        if price < minval {
+            minval = price
+        } else {
+            maxprofit = max(maxprofit, price - minval)
+        }
+    }
+
+    return maxprofit
+}
+
+func main() {
+    arr := []int{7,1,5,3,6,4} // example
+    ans := maxProfit(arr)
+    fmt.Printf("%v", ans)
+}
