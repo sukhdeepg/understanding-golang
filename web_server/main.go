6e7fc1d8acb237bcd47f310458d6063df89a6bdf
sukhdeep <gillsukhdeep452@gmail.com>
2022-09-06 23:15:51 +0530

    initial hello handler

--- web_server/main.go
+++ web_server/main.go
@@ -6,10 +6,15 @@ import(
 	"net/http"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	
+}
+
 func main(){
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
 	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
 
 	fmt.Println("Starting server ...")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
