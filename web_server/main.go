bf6ebfb5f82f80ab3bbc2a68c398f639a42448e8
sukhdeep <gillsukhdeep452@gmail.com>
2022-09-07 11:47:46 +0530

    completed hello handler

--- web_server/main.go
+++ web_server/main.go
@@ -6,8 +6,21 @@ import(
 	"net/http"
 )
 
+// ResponseWriter: This is the mechanism used for sending responses to any connected HTTP clients. It's also how response headers are set.
+// http.Request: is a pointer to an http.Request. It's how data is retrieved from the web request. For example, the details from a form submission can be accessed through the request pointer.
+// ref: https://www.honeybadger.io/blog/go-web-services/
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	
+	if r.URL.Path != "hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "method not allowed", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "Hello!")
 }
 
 func main(){
