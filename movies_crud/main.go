6a19ee0b64fe8b331d4b2545ce5484d352330f0a
sukhdeep <gillsukhdeep452@gmail.com>
2022-10-13 18:22:08 +0530

    setting up main.go

---
+++ movies_crud/main.go
@@ -0,0 +1,23 @@
+package main
+
+import(
+	"fmt"
+	"log"
+	"encoding/json"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"github.com/gorilla/mux"
+)
+
+type Movie struct{
+	ID string `json:"id"`
+	Isbn string `json:"isbn"`
+	Title string `json:"title"`
+	Director *Director `json:"director"` 
+}
+
+type Director struct{
+	Firstname string `json:"firstname"`
+	Lastname string `json:"lastname"`
+}
